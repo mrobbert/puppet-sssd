1a41c63850a02d3b33089ddd16a2fbfc428848c0
Michael Robbert <mrobbert@mines.edu>
2012-07-16 15:24:12 -0600

    Add Debian family support

--- manifests/params.pp
+++ manifests/params.pp
@@ -6,6 +6,12 @@ class sssd::params {
             $sssd_tools_package_name = 'sssd-tools'
             $sssd_service_name = 'sssd'
         }
+	/Debian/: {
+            $sssd_package_name = 'sssd'
+            $sssd_client_package_name = 'sssd-client'
+            $sssd_tools_package_name = 'sssd-tools'
+            $sssd_service_name = 'sssd'
+	}
 
         default: {
             fail("Unsupported OS Family: $osfamily")

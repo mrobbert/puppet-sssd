9746b6d172f8d0aab1a04ce2c1d46631c886f70f
Michael Robbert <mrobbert@mines.edu>
2012-07-17 15:49:27 -0600

    Adding parameters

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,14 @@
-class sssd {
-    include sssd::params, sssd::install, sssd::service
+class sssd (
+	$domainname = "default",
+	$filter_groups = "root",
+	$filter_users = "root",
+	$ldap_uri = "",
+	$ldap_search_base = "",
+	$krb5_realm = "",
+	$krb5_server = "",
+	$krb5_kdcip = "",
+	$krb5_kpasswd = "",
+	)
+{
+    include sssd::params, sssd::install, sssd::config, sssd::service
 }

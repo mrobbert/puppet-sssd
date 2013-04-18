7640e1db44c297e08ef5121afa6bff6cb1f8277f
Michael Robbert <mrobbert@mines.edu>
2013-04-18 15:59:54 -0600

    Add more parameters and templates

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,21 +1,56 @@
+##
+# Class sssd::config - Configures the SSSD package
 class sssd::config {
-	file { "/etc/nsswitch.conf":
-		ensure	=> present,
-		owner	=> root,
-		group	=> root,
-		mode	=> 0644,
-		content	=> template("sssd/nsswitch.conf.erb"),
-		require	=> Class["sssd::install"],
-		notify	=> Class["sssd::service"],
-	}
+  File {
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    require => Class['sssd::install'],
+    notify  => Class['sssd::service'],
+  }
+  file { '/etc/nsswitch.conf':
+    content => template('sssd/nsswitch.conf.erb'),
+  }
 
-	file { "/etc/sssd/sssd.conf":
-		ensure	=> present,
-		owner	=> root,
-		group	=> root,
-		mode	=> 0600,
-		content	=> template("sssd/sssd.conf.erb"),
-		require	=> Class["sssd::install"],
-		notify	=> Class["sssd::service"],
-	}
+  file { '/etc/sssd/sssd.conf':
+    mode    => '0600',
+    content => template('sssd/sssd.conf.erb'),
+  }
+
+  case $::osfamily {
+    /RedHat/: {
+      file { '/etc/pam.d/system-auth-ac':
+        content => template('sssd/system-auth-ac.erb'),
+      }
+      file { '/etc/pam.d/system-auth':
+        ensure  => link,
+        target  => '/etc/pam.d/system-auth-ac',
+      }
+      file { '/etc/pam.d/password-auth-ac':
+        content => template('sssd/password-auth-ac.erb'),
+      }
+      file { '/etc/pam.d/password-auth':
+        ensure  => link,
+        target  => '/etc/pam.d/password-auth-ac',
+      }
+    }
+    /Debian/: {
+      file { '/etc/pam.d/common-account':
+        content => template('sssd/common-account.erb'),
+      }
+      file { '/etc/pam.d/common-auth':
+        content => template('sssd/common-auth.erb'),
+      }
+      file { '/etc/pam.d/common-password':
+        content => template('sssd/common-password.erb'),
+      }
+      file { '/etc/pam.d/common-session':
+        content => template('sssd/common-session.erb'),
+      }
+    }
+    default: {
+      fail("Module sssd is not supported on ${::osfamily}")
+    }
+  }
 }
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,14 +1,23 @@
+##
+# Class sssd - Installs and configures the SSSD package for authentication
+# and authorization
 class sssd (
-	$domainname = "default",
-	$filter_groups = "root",
-	$filter_users = "root",
-	$ldap_uri = "",
-	$ldap_search_base = "",
-	$krb5_realm = "",
-	$krb5_server = "",
-	$krb5_kdcip = "",
-	$krb5_kpasswd = "",
-	)
+  $domainname = 'default',
+  $filter_groups = 'root',
+  $filter_users = 'root',
+  $ldap_uri = '',
+  $ldap_search_base = '',
+  $ldap_user_search_base = '',
+  $ldap_group_search_base = '',
+  $ldap_tls_cacertdir = '',
+  $ldap_tls_reqcert = '',
+  $krb5_realm = '',
+  $krb5_server = '',
+  $krb5_validate = true,
+  $krb5_kdcip = '',
+  $krb5_kpasswd = '',
+  $enable_pam = false,
+  )
 {
     include sssd::params, sssd::install, sssd::config, sssd::service
 }
